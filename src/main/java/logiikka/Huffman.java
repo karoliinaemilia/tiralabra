eefbed120bb6a8f7827f3200d7a093a3603ed65c
Karoliina <karoliina.koivu@helsinki.fi>
2022-05-21 23:38:28 +0300

    checkstyle

---
+++ src/main/java/logiikka/Huffman.java
@@ -0,0 +1,11 @@
+package logiikka;
+
+public class HuffmanNode implements Comparable<HuffmanNode> {
+    char merkki;
+    int frekvenssi;
+    HuffmanNode vasen;
+    HuffmanNode oikea;
+}
+
+public class Huffman {
+}
--- src/test/java/ui/testTest.java
+++ src/test/java/ui/testTest.java
@@ -1,4 +1,4 @@
-package UI;
+package ui;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
